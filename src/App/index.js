cc78775545045afa09d604955ce8d5e439a3afc8
graemegeorge <graemelgeorge@gmail.com>
2020-09-15 13:43:27 +0100

    Add application setup files

---
+++ src/App/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Nav, NavItem } from "./components/Navigation";
+import { Countries } from "./Countries";
+import { Home } from "./Home";
+import { Country } from "./Countries/Country";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/countries" exact component={Countries} />
+          <Route path="/countries/:code" component={Country} />
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+function Navigation() {
+  return (
+    <Nav>
+      <NavItem to="/">Home</NavItem>
+      <NavItem to="/countries">Countries</NavItem>
+    </Nav>
+  );
+}
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    margin: 0 auto;
+    font-size: 14px;
+    font-family: Arial, Helvetica Neue, Helvetica, sans-serif; 
+  }
+
+  a {
+    text-decoration: none;
+  }
+`;
+
+export { App, Navigation as AppNav };
